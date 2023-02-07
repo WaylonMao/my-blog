98bddd5c3da1fc6d14e7a3bc0e24c6e87552edf5
WaylonM <weilong.mao@edu.sait.ca>
2023-02-06 23:32:49 -0700

    Designed login and register.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,60 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './style.scss';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+      {
+        path: '/post/:postId',
+        element: <Single />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+]);
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Write.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const write = () => {
+  return (
+    <div>write</div>
+  )
+}
+
+export default write
