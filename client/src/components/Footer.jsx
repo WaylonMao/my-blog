84755f398c2e8c732dbeb14c539e2bafdcf18326
WaylonM <weilong.mao@edu.sait.ca>
2023-02-07 23:56:30 -0700

    Designed navbar and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Logo from '../img/logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="" />
+      <span>By Weilong Mao - Made with <b>React.js</b></span>
+
+    </footer>
   )
 }
 
