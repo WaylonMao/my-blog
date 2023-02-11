794b380e9f24722cc652727f408111b77e89dbb4
WaylonM <weilong.mao@edu.sait.ca>
2023-02-10 23:57:21 -0700

    Added context

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../img/logo.png';
+import { AuthContext } from '../context/authContext';
 
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <div className='container'>
@@ -28,8 +31,14 @@ const Navbar = () => {
           <Link className='link' to='/?cat=food'>
             <h6>FOOD</h6>
           </Link>
-          <span>userName</span>
-          <span>Logout</span>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className='link' to='/login'>
+              Login
+            </Link>
+          )}
           <span className='write'>
             <Link className='link' to='/write'>
               Write
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -13,6 +14,8 @@ const Login = () => {
 
   const navigate = useNavigate();
 
+  const { login } = useContext(AuthContext);
+
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -20,7 +23,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('/auth/login', inputs);
+      await login(inputs);
       navigate('/');
     } catch (err) {
       setError(err.response.data);
